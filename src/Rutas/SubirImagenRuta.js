aceb875419eb99be0a826136547cf387255f1fe0
prosystem-pro <prosystem490@gmail.com>
2025-05-15 08:26:21 -0600

    Se protegió la ruta de imagen

--- src/Rutas/SubirImagenRuta.js
+++ src/Rutas/SubirImagenRuta.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const { SubirImagen } = require("../Controladores/SubirImagenControlador");
+const Tabla = 'Imagen'
 const { Subir } = require("../FuncionIntermedia/SubirImagen");
+const VerificarToken = require("../FuncionIntermedia/VerificarToken");
+const VerificarPermisos = require("../FuncionIntermedia/VerificarPermisos");
+
 
 const RutaAlmacenamiento = Router();
-RutaAlmacenamiento.post("/subir-imagen", Subir.single("Imagen"), SubirImagen);
+RutaAlmacenamiento.post("/subir-imagen",VerificarToken, VerificarPermisos('SubirImagen', Tabla), Subir.single("Imagen"), SubirImagen);
 
 module.exports = RutaAlmacenamiento;
