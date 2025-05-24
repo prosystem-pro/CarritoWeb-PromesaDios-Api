0bea436a8596053353a7c29c0d7091f0769fcff5
prosystem-pro <prosystem490@gmail.com>
2025-05-24 17:59:47 -0600

    Se liberó la ruta de buscar de producto

--- src/Rutas/ProductoRuta.js
+++ src/Rutas/ProductoRuta.js
@@ -8,7 +8,7 @@ const VerificarPermisos = require('../FuncionIntermedia/VerificarPermisos');
 
 Router.get(`/${Modelo}/listado`, Listado);
 Router.get(`/${Modelo}/:Codigo`,VerificarToken,VerificarPermisos('Ver',Tabla), ObtenerPorCodigo);
-Router.get(`/${Modelo}/buscar/:TipoBusqueda/:ValorBusqueda`,VerificarToken,VerificarPermisos('Buscar',Tabla), Buscar);
+Router.get(`/${Modelo}/buscar/:TipoBusqueda/:ValorBusqueda`, Buscar);
 Router.post(`/${Modelo}/crear`, VerificarToken,VerificarPermisos('Crear',Tabla),Crear);
 Router.put(`/${Modelo}/editar/:Codigo`, VerificarToken,VerificarPermisos('Editar',Tabla), Editar);
 Router.delete(`/${Modelo}/eliminar/:Codigo`, VerificarToken,VerificarPermisos('Eliminar',Tabla),  Eliminar);
